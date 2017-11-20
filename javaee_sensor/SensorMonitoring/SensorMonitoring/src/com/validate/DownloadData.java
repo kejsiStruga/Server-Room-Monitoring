e10714431cc42366831b72f3533b455f3d868b6b
Kejsi Struga <kejsi.struga@fshnstudent.info>
2017-11-20 10:01:56 +0100

    Update DownloadData.java

--- javaee_sensor/SensorMonitoring/SensorMonitoring/src/com/validate/DownloadData.java
+++ javaee_sensor/SensorMonitoring/SensorMonitoring/src/com/validate/DownloadData.java
@@ -39,96 +39,94 @@ public class DownloadData extends HttpServlet {
     private DynamoDB dynamoDB;
     private String tableName = "serverRoomSensor";
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-		throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+ 		throws ServletException, IOException {
 		doPost(request, response);		
-	}
-	
-	@SuppressWarnings("deprecation")
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+    } 
+
+    @SuppressWarnings("deprecation")
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 		throws ServletException, IOException {
-		
-		AWSCredentials credentials = new ProfileCredentialsProvider("default").getCredentials();      
-		dynamoDBClient = new AmazonDynamoDBClient(credentials);
-		Region usWest2 = Region.getRegion(Regions.US_WEST_2);
-		dynamoDBClient.setRegion(usWest2);
-		dynamoDB = new DynamoDB(dynamoDBClient);
 
-		//get user equest for start day and end day
-		String start_date = request.getParameter("date");
-		String end_date = request.getParameter("end_date");
+	AWSCredentials credentials = new ProfileCredentialsProvider("default").getCredentials();      
+	dynamoDBClient = new AmazonDynamoDBClient(credentials);
+	Region usWest2 = Region.getRegion(Regions.US_WEST_2);
+	dynamoDBClient.setRegion(usWest2);
+	dynamoDB = new DynamoDB(dynamoDBClient);
 
-		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-		Date date_start, date_end;
-		Calendar start_cal = Calendar.getInstance();
-		Calendar end_cal = start_cal;
-		try {
-			date_start = dateFormat.parse(start_date);
-			date_end = dateFormat.parse(end_date); 
-			end_cal =start_cal;
-		    start_cal.setTime(date_start);
-		    end_cal.setTime(date_end);			
+	//get user equest for start day and end day
+	String start_date = request.getParameter("date");
+	String end_date = request.getParameter("end_date");
 
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+	Date date_start, date_end;
+	Calendar start_cal = Calendar.getInstance();
+	Calendar end_cal = start_cal;
+	try {
+		date_start = dateFormat.parse(start_date);
+		date_end = dateFormat.parse(end_date); 
+		end_cal =start_cal;
+	    start_cal.setTime(date_start);
+	    end_cal.setTime(date_end);			
 
-		String lower_bound = String.valueOf(new Date(start_cal.getTimeInMillis()).toString());
-		String upper_bound = String.valueOf(new Date(end_cal.getTimeInMillis()).toString());
-		
-		PrintWriter out = response.getWriter();
-		response.setContentType("application/ms-excel");
-		response.setHeader("Content-Disposition", "attachement; filename=sensor-data-"+append_date+".csv");
-	
-		//RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/Copy/Dashboard1.jsp");
-		//dispatcher.forward(request, response);
-		
-		Table table = dynamoDB.getTable(tableName);
-		HashMap<String, String> nameMap = new HashMap<String, String>();
-		nameMap.put("#key", "key");        
-		nameMap.put("#timestamp", "timestamp");        
-		HashMap<String, Object> valueMap = new HashMap<String, Object>();
-		valueMap.put(":key", "sensordata");        
-		valueMap.put(":start_date", lower_bound);        
-		valueMap.put(":end_date", upper_bound);        
-		QuerySpec spec=new QuerySpec();
-		spec.withKeyConditionExpression("#key = :key and #timestamp BETWEEN :start_date AND :end_date");
-		spec.withNameMap(nameMap);
-		spec.withValueMap(valueMap);
-
-		ItemCollection<QueryOutcome> items = table.query(spec);
-		Iterator<Item> iterator = items.iterator();
-
-		Item item = null;
-		Gson gson = new Gson();
-
-		out.print(FILE_HEADER.toString());
-		out.print(NEW_LINE);
-
-        
-		 while (iterator.hasNext()) {
-		        	
-			item = iterator.next();
-			String sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new Date(Long.parseLong(item.getString("timestamp"))));
-
-			//Convert the ordered map into an ordered string.
-			String json = gson.toJson(item.get("payload"), LinkedHashMap.class);            
-			JSONObject value;
-			try {
-				value = new JSONObject(json);
-				//value.remove("createdAt");
-				Iterator<?> keys = value.keys();
-				while(keys.hasNext() ) {
-				   String key = (String)keys.next();		            	
-				}        
-				out.print("");
-				out.print(NEW_LINE); 
-
-			} catch (JSONException e) { e.printStackTrace(); }                           
-		}        
-        
-		out.flush();	
+	} catch (ParseException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
 	}
+
+	String lower_bound = String.valueOf(new Date(start_cal.getTimeInMillis()).toString());
+	String upper_bound = String.valueOf(new Date(end_cal.getTimeInMillis()).toString());
+
+	PrintWriter out = response.getWriter();
+	response.setContentType("application/ms-excel");
+	response.setHeader("Content-Disposition", "attachement; filename=sensor-data-"+append_date+".csv");
+
+	//RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/Copy/Dashboard1.jsp");
+	//dispatcher.forward(request, response);
+
+	Table table = dynamoDB.getTable(tableName);
+	HashMap<String, String> nameMap = new HashMap<String, String>();
+	nameMap.put("#key", "key");        
+	nameMap.put("#timestamp", "timestamp");        
+	HashMap<String, Object> valueMap = new HashMap<String, Object>();
+	valueMap.put(":key", "sensordata");        
+	valueMap.put(":start_date", lower_bound);        
+	valueMap.put(":end_date", upper_bound);        
+	QuerySpec spec=new QuerySpec();
+	spec.withKeyConditionExpression("#key = :key and #timestamp BETWEEN :start_date AND :end_date");
+	spec.withNameMap(nameMap);
+	spec.withValueMap(valueMap);
+
+	ItemCollection<QueryOutcome> items = table.query(spec);
+	Iterator<Item> iterator = items.iterator();
+
+	Item item = null;
+	Gson gson = new Gson();
+
+	out.print(FILE_HEADER.toString());
+	out.print(NEW_LINE);
+
+	 while (iterator.hasNext()) {
+
+		item = iterator.next();
+		String sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new Date(Long.parseLong(item.getString("timestamp"))));
+
+		//Convert the ordered map into an ordered string.
+		String json = gson.toJson(item.get("payload"), LinkedHashMap.class);            
+		JSONObject value;
+		try {
+			value = new JSONObject(json);
+			//value.remove("createdAt");
+			Iterator<?> keys = value.keys();
+			while(keys.hasNext() ) {
+			   String key = (String)keys.next();		            	
+			}        
+			out.print("");
+			out.print(NEW_LINE); 
+
+		} catch (JSONException e) { e.printStackTrace(); }                           
+	}        
+	out.flush();	
+  }
 	
 }
